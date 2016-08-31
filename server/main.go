6e4102de1aeede1af03d8c58b57c91a09a1db969
Mr-Bruno <bru.dominguez@gmail.com>
2016-08-31 01:22:30 +0100

    Removing the hander functions from the main file
    The handler functions have been implemented in their own handler file.

--- server/main.go
+++ server/main.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/Mr-Bruno/goencryption/serverutil"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to goencryption")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/StoreData", StoreData)
-	http.HandleFunc("/GetData", GetData)
+	http.HandleFunc("/", serverutil.Handler)
+	http.HandleFunc("/StoreData", serverutil.StoreData)
+	http.HandleFunc("/GetData", serverutil.GetData)
 	http.ListenAndServe(":8080", nil)
-
-}
-
-//StoreData manages the encryption and storage of the data.
-func StoreData(w http.ResponseWriter, r *http.Request) {
-
-}
-
-//GetData manages fetching the data, its decryption and sending it back
-func GetData(w http.ResponseWriter, r *http.Request) {
 
 }
